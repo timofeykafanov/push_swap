396e27080045f52707bdc28a9c4fd594cf4a8bfb
Timofey <74354936+timofeykafanov@users.noreply.github.com>
2024-05-20 17:08:32 +0000

    add push

--- push.c
+++ push.c
@@ -6,7 +6,20 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 18:52:23 by codespace         #+#    #+#             */
-/*   Updated: 2024/05/19 20:22:43 by codespace        ###   ########.fr       */
+/*   Updated: 2024/05/20 17:07:53 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+void    push(t_list **dst, t_list **src)
+{
+    t_list *first;
+
+    if (!(*src))
+        return ;
+    first = *src;
+    *src = (*src)->next;
+    first->next = *dst;
+	*dst = first;
+}
